5d175de540d4111731a1f42e9236edba022d3fab
Nayan <nayanraj8766@gmail.com>
2023-03-28 11:07:59 +0530

    test PR

--- server/src/db/conn.js
+++ server/src/db/conn.js
@@ -5,6 +5,7 @@ Sequelize.DATE.prototype._stringify = function _stringify(date, options) {
   date = this._applyTimezone(date, options);
 
   // Z here means current timezone, _not_ UTC
+  // Z here means current timezone, _not_ UTC
   // return date.format('YYYY-MM-DD HH:mm:ss.SSS Z');
   return date.format("YYYY-MM-DD HH:mm:ss.SSS");
 };
